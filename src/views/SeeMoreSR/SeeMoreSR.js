21b8751c6a69b8941dd5f97814c170b908d00d75
Gizen <hoangdaheodaheo@gmail.com>
2021-06-03 13:46:57 +0700

    update seemore search tools

--- src/views/SeeMoreSR/SeeMoreSR.js
+++ src/views/SeeMoreSR/SeeMoreSR.js
@@ -1,6 +1,6 @@
 import React, { useState,useEffect } from "react";
 import "./SeeMoreStudent.scss";
-
+import InputLabel from '@material-ui/core/InputLabel';
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import Table from "components/Table/Table.js";
@@ -26,32 +26,67 @@ const SeeMore = () => {
   const classes = useStyles();
   const [sr, setSr] = useState();
   const [dataInput,setDataInput]= useState();
+  const [date,setDate] = useState();
   useEffect(() => {
     ResultService.getResultWarning().then((res) => {
-      if(dataInput){
+      if(dataInput && date === undefined){
         setSr((res.data).filter((val)=>{
           console.log(val.id);
           return val.id == dataInput;
-        }))
-      }else{
+        }));
+      }else if(dataInput && date){
+        setSr((res.data).filter((val)=>{
+          console.log(val.id);
+          return val.id == dataInput && val.time.split(' ')[0] == date;
+        }));
+      }else if ((dataInput === undefined || dataInput==='') && date){
+        setSr((res.data).filter((val)=>{
+          console.log(val.id);
+          return val.time.split(' ')[0] == date;
+        }));
+      }
+
+      else{
         console.log(res.data);
         setSr(res.data);
       }
 
     });
 
-  }, [dataInput]);
+  }, [dataInput,date]); 
   return (
+    
     <div>
+    <GridItem xs={12} sm={12} md={4}>
+        <Card>
+
+            <CardBody>
+           <InputLabel style={{color: "green"}} id="demo-simple-select-label">ID</InputLabel>
+              <Input
+                    style={{width: "100%"}}
+                    type="number" 
+                    onChange={(event) => {setDataInput(event.target.value);}}
+                  />
+                
+           </CardBody>
+            <CardBody>
+           
+           <InputLabel style={{color: "green"}} id="demo-simple-select-label">Date</InputLabel>
+           <Input style={{width: "100%"}} type="date" onChange={(event) => {setDate(event.target.value);}}/>
+           
+           </CardBody>
+        </Card>
+  
+        </GridItem>
       <GridItem xs={12} sm={12} md={19}>
-      <div>
+      {/* <div>
           <label className={classes.cardTitleBlack}>Student ID&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
             <Input
               style={styles.input}
               type="number" 
               onChange={(e) =>{setDataInput(e.target.value)}}
             />
-          </div>
+          </div> */}
         <Card>
           <CardHeader color="danger" style={{ marginBottom: -30 }}>
             <h4 className={classes.cardTitleWhite}>Hyperthemia</h4>
